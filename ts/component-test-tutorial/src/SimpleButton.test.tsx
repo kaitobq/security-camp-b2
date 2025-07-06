96d9ca3fc0d59c06d7a9ea3505d2f249eaa5e93d
kaitobq <kaitobq520@gmail.com>
2025-07-06 14:39:51 +0900

    finish component-test

---
+++ ts/component-test-tutorial/src/SimpleButton.test.tsx
@@ -0,0 +1,21 @@
+import { render, screen, act } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import { SimpleButton } from "./SimpleButton";
+
+test("ボタンをクリックするとON/OFFの表示が切り替わる", async () => {
+  const user = userEvent.setup();
+  render(<SimpleButton />);
+  const simpleButton = screen.getByRole("button");
+  expect(simpleButton).toHaveTextContent("OFF");
+  
+  await act(async () => {
+    await user.click(simpleButton);
+  });
+  
+  expect(simpleButton).toHaveTextContent("ON");
+});
+
+test("描画されてすぐはOFFと表示されている", () => {
+  const view = render(<SimpleButton />);
+  expect(view.container).toMatchSnapshot();
+});
